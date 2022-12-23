ad1f4c50d5df42edfc8b75994236b5959f420a44
Halieeim <ahmedlpl86@gmail.com>
2022-12-23 03:26:15 +0200

    initial commit

--- src/Elgamal.java
+++ src/Elgamal.java
@@ -1,19 +1,26 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Elgamal {
-    long p,a,XA;
+    long p,a,XA,k;
     Random rand = new Random();
-    public long selectP(){
+    public void selectP(){              // selecting random value for p that should be prime and greater than the message that will be encrypted
         do {
-            p = rand.nextLong(129, (long) (Math.pow(2,64)-1));
+            p = rand.nextLong(129, 65000);
         } while (!isPrime(p));
     }
-    public long selecta(){
-        do {
-            a = rand.nextLong(2, p);
-        } while (!isPrime(a));
+    public void selecta(){              // selecting random alpha < p
+        a = rand.nextLong(2, p);
+    }
+
+    public void selectXA(){             // selecting random XA < p
+        XA = rand.nextLong(2,p);
+    }
+
+    public void selectk(){              // selecting random k < p
+        k = rand.nextLong(2,p);
     }
     public long square_Multiply(long a, long b, long n){
         long result = 1;
@@ -57,26 +64,41 @@ public class Elgamal {
     }
 
     public ArrayList<long[]> keyGeneration() {
-        long[] res1 = new long[2];
-        long[] res2 = new long[2];
         ArrayList<long[]> Result = new ArrayList<>();
-        long n, r, e, gcd, d;
-        Scanner input = new Scanner(System.in);
-
+        long YA;
+        selectP();
+        selecta();
+        selectXA();
+        YA = square_Multiply(a,XA,p);
+        System.out.println("Public Key {p,a,YA} = {" + p + "," + a + "," + YA + "}");
+        System.out.println("Private Key {XA} = {" + XA + "}");
+        long[] pub = {p,a,YA};
+        long[] pri = {XA};
+        Result.add(pub);
+        Result.add(pri);
         return Result;
     }
 
-    public long[] encrypt(String text, long[] publicKey){
-        long key, n;
-        long[] cipher = new long[text.length()];
-
+    public ArrayList<long[]> encrypt(String text, long[] publicKey){
+        long K,c1,c2;
+        long[] c = new long[2];
+        ArrayList<long[]> cipher = new ArrayList<>();
+        selectk();
+        K = square_Multiply(publicKey[2],k,p);
+        c1 = square_Multiply(a,k,p);
+        for (int i = 0; i < text.length(); i++){
+            c2 = square_Multiply(K * text.charAt(i),1,p);       // c2 = KM mod p
+            cipher.add(new long[]{c1, c2});
+        }
         return cipher;
     }
-
-    public long[] decrypt(long[] cipher, long[] privateKey){
-        long key, n, xp, xq, dp, dq, cp, cq;
-        long[] plainText = new long[cipher.length];
-
+    public long[] decrypt(ArrayList<long[]> cipher, long[] privateKey){
+        long[] plainText = new long[cipher.size()];
+        long Kinv = square_Multiply(cipher.get(0)[0],XA,p);                                 // K = c1^XA mod p
+        Kinv = EEA(Kinv,p);
+        for (int i = 0; i < cipher.size(); i++){
+            plainText[i] = square_Multiply(cipher.get(i)[1] * Kinv,1,p);              // m = c2 * K^-1 mod p
+        }
         return plainText;
     }
     public static void main(String[] args) {
@@ -84,13 +106,22 @@ public class Elgamal {
         System.out.println("Enter Plaintext to encrypt:");
         Scanner input = new Scanner(System.in);
         String plainText = input.nextLine();
-        long[] t = new long[plainText.length()];
-
         /* **************************************************************Key_Generation************************************************************** */
-
+        Elgamal elgamal = new Elgamal();
+        ArrayList<long[]> keys = elgamal.keyGeneration();
+        long[] publicKey = keys.get(0);
+        long[] privateKey = keys.get(1);
         /* ****************************************************************Encryption**************************************************************** */
-
+        ArrayList<long[]> encrypted = elgamal.encrypt(plainText,publicKey);
+        System.out.println("Encryption:\n" + Arrays.deepToString(encrypted.toArray()));
         /* ****************************************************************Decryption**************************************************************** */
-
+        long[] decrypted = elgamal.decrypt(encrypted,privateKey);
+        char letter;
+        String text = "";
+        for (int i = 0; i < decrypted.length; i++){
+            letter = (char) decrypted[i];
+            text = text.concat(String.valueOf(letter));
+        }
+        System.out.println("\n" + text);
     }
 }
